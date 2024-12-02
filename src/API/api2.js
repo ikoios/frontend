26c9351861d01c5b379b06b2e973c519130017c2
ikoios <sr.sullivan.richard@gmail.com>
2024-12-02 14:09:01 +0100

    fetchs user api, add newUser form
    Co-authored-by: Laitrez <Laitrez@users.noreply.github.com>

--- src/API/api2.js
+++ src/API/api2.js
@@ -1,9 +1,29 @@
 import axios from "axios";
 
-export const fetchUsers2 = async () => {
+export const fetchUsersGet = async () => {
   const response = await axios({
-    method: 'GET',
-    url: `http://127.0.0.1:8000/users`,
+    method: "GET",
+    url: "http://127.0.0.1:8000/users",
   });
-   return response.data;
-}
+
+  return response.data;
+};
+
+export const fetchCurrentUser = async (id) => {
+  const response = await axios({
+    method: "GET",
+    url: `http://127.0.0.1:8000/users/${id}`,
+  });
+
+  return response.data;
+};
+
+export const fetchUsersPost = async (user) => {
+  const response = await axios({
+    method: "POST",
+    url: "http://127.0.0.1:8000/newUser",
+    data: user,
+  });
+
+  return response;
+};
--- src/components/FormNewUser.jsx
+++ src/components/FormNewUser.jsx
@@ -1,63 +1,136 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import Row from "react-bootstrap/Row";
-// import * as formik from "formik";
-// import * as yup from "yup";
+import * as formik from "formik";
+import * as yup from "yup";
 import { useEffect, useState } from "react";
-// import fetchUsers from "../API/api";
-import { fetchUsers2 } from "../API/api2";
+import { fetchUsersGet, fetchCurrentUser, fetchUsersPost } from "../API/api2";
+
 const FormNewUser = () => {
-  // const { Formik } = formik;
-
-  // const schema = yup.object().shape({
-  // })
-  //     return (
-  //         <>
-  //         <Formik
-  //         validationSchema={schema}
-  //         onSubmit={console.log}
-  //         initialValues={{
-  //         }}
-  //       >
-  //         {({ handleSubmit, handleChange, values, touched, errors }) => (
-
-  //         )}
-  //     </Formik>
-
-  //     </>
-  // )
-
-
-
   const [users, setUsers] = useState([]);
-  const [error, setError]= useState(false);
+  const [error, setError] = useState(false);
 
+  async function fetchs(user) {
+    // const usersApi = await fetchUsersGet(method);
 
-  async function testFetch(method, corps) {
-    
-    const userApi2= corps ? fetchUsersPost(corps) : method === 'Delete' ? fetchUsersDelete() : fetchUsersGet(); 
-   
-    const usersApi = await fetchUsers2(method, comp);
-    setUsers(usersApi);
-  
-  } 
-  
+    // if (user) {
+    await fetchUsersPost(user);
+    // } else {
+    //   const currentUser = await fetchCurrentUser(1);
+
+    //   setUsers(currentUser);
+    // }
+
+    // const currentUser = await fetchCurrentUser(method);
+
+    // await fetchUsersPost(method, user);
+
+    // console.log("userAPI: ", usersApi);
+  }
+
   useEffect(() => {
     //soumission du store
-    testFetch();
-    // return()=> fin de soumission
-  }, []);
-  
-  console.log('user : ',users);
-  
+    if (users.length > 0) fetchs(users);
+  }, [users]);
+
+  const { Formik } = formik;
+
+  const schema = yup.object().shape({
+    first_name: yup.string().required(),
+    last_name: yup.string().required(),
+    phone: yup.string().required(),
+    mail: yup.string().required(),
+    identifier: yup.string().required(),
+    password: yup.string().required(),
+  });
+
   return (
     <>
-      <ul>
-        {users.map((user, i) => {
-          <li key={i}>{user}</li>;
-        })}
-      </ul>
+      <Formik
+        validationSchema={schema}
+        onSubmit={(values) => {
+          setUsers(values);
+        }}
+        initialValues={{
+          first_name: "",
+          last_name: "",
+          phone: "",
+          mail: "",
+          identifier: "",
+          password: "",
+        }}
+      >
+        {({ handleSubmit, handleChange, values, errors }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Form.Group controlId="validationFormik01">
+              <Form.Label>Prénom</Form.Label>
+              <Form.Control
+                type="text"
+                name="first_name"
+                value={values.first_name}
+                onChange={handleChange}
+                isInvalid={!!errors.first_name}
+              />
+              <Form.Control.Feedback>{errors.first_name}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="validationFormik02">
+              <Form.Label>Nom</Form.Label>
+              <Form.Control
+                type="text"
+                name="last_name"
+                value={values.last_name}
+                onChange={handleChange}
+                isInvalid={!!errors.last_name}
+              />
+              <Form.Control.Feedback>{errors.last_name}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="validationFormik02">
+              <Form.Label>Téléphone</Form.Label>
+              <Form.Control
+                type="text"
+                name="phone"
+                value={values.phone}
+                onChange={handleChange}
+                isInvalid={!!errors.phone}
+              />
+              <Form.Control.Feedback>{errors.phone}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="validationFormik02">
+              <Form.Label>Mail</Form.Label>
+              <Form.Control
+                type="text"
+                name="mail"
+                value={values.mail}
+                onChange={handleChange}
+                isInvalid={!!errors.mail}
+              />
+              <Form.Control.Feedback>{errors.mail}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="validationFormik02">
+              <Form.Label>Identifiant</Form.Label>
+              <Form.Control
+                type="text"
+                name="identifier"
+                value={values.identifier}
+                onChange={handleChange}
+                isInvalid={!!errors.identifier}
+              />
+              <Form.Control.Feedback>{errors.identifier}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="validationFormik02">
+              <Form.Label>Mot de pass</Form.Label>
+              <Form.Control
+                type="text"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                isInvalid={!!errors.password}
+              />
+              <Form.Control.Feedback>{errors.password}</Form.Control.Feedback>
+            </Form.Group>
+            <Button type="submit">Submit form</Button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 };
